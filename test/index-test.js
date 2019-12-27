70978744088fac7d88dcb1223b799f0341fb797c
Breaun <pynkylynn@gmail.com>
2019-12-27 19:06:37 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,7 +4,7 @@ describe('Arrays', function() {
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
     })
   })
-  const new_arrays= old_array.concat 
+  const new_arrays= old_array.concat(kittens) 
 
   describe('destructivelyAppendKitten(name)', function() {
     it('appends a kitten to the end of the kittens array', function() {
